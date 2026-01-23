1b603c9306b90410491fa8c621ca22b45e36e986
Vadim Kononov <vadim@konoson.com>
2026-01-23 13:33:59 -0600

    Initial commit.

---
+++ lib/devise/cloudflare/turnstile.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "turnstile/version"
+
+module Devise
+  module Cloudflare
+    module Turnstile
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/devise/cloudflare/turnstile/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Devise
+  module Cloudflare
+    module Turnstile
+      VERSION = "0.1.0"
+    end
+  end
+end
