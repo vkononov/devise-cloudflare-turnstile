26115d712905f5f6862e3151303971136c713cc4
Vadim Kononov <vadim@konoson.com>
2026-07-25 22:32:49 -0500

    Leave layout setup to the user during install
    The installer used to edit application.html.erb to add the Turnstile
    helper tags, but that only worked for a default ERB layout and silently
    did nothing for other setups (like HAML), leaving the widget missing
    with no error. The installer now just prints the two tags to add, and
    the README documents this manual step and is simplified overall.

--- test/generators/devise_cloudflare_turnstile/install_generator_test.rb
+++ test/generators/devise_cloudflare_turnstile/install_generator_test.rb
@@ -10,14 +10,9 @@ class InstallGeneratorTest < Rails::Generators::TestCase
     assert_raises(SystemExit) { run_generator }
   end
 
-  def test_creates_initializer_and_injects_layout_helpers # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  def test_creates_initializer
     FileUtils.mkdir_p(File.join(destination_root, 'config/initializers'))
-    FileUtils.mkdir_p(File.join(destination_root, 'app/views/layouts'))
     File.write(File.join(destination_root, 'config/initializers/devise.rb'), "# devise\n")
-    File.write(
-      File.join(destination_root, 'app/views/layouts/application.html.erb'),
-      "<!DOCTYPE html>\n<html>\n  <head>\n    <title>App</title>\n  </head>\n  <body></body>\n</html>\n"
-    )
 
     run_generator
 
@@ -26,36 +21,19 @@ class InstallGeneratorTest < Rails::Generators::TestCase
       assert_match(/config\.site_key/, content)
       assert_match(/config\.secret_key/, content)
     end
-
-    assert_file 'app/views/layouts/application.html.erb' do |content|
-      assert_match(/devise_turnstile_meta_tag/, content)
-      assert_match(/devise_turnstile_scripts/, content)
-    end
   end
 
-  def test_skips_layout_injection_when_already_configured # rubocop:disable Metrics/AbcSize
+  def test_leaves_the_layout_untouched
     FileUtils.mkdir_p(File.join(destination_root, 'config/initializers'))
     FileUtils.mkdir_p(File.join(destination_root, 'app/views/layouts'))
     File.write(File.join(destination_root, 'config/initializers/devise.rb'), "# devise\n")
-    File.write(
-      File.join(destination_root, 'app/views/layouts/application.html.erb'),
-      "<head>\n    <%= devise_turnstile_meta_tag %>\n</head>\n"
-    )
+    layout = "<!DOCTYPE html>\n<html>\n  <head>\n    <title>App</title>\n  </head>\n  <body></body>\n</html>\n"
+    File.write(File.join(destination_root, 'app/views/layouts/application.html.erb'), layout)
 
     run_generator
 
     assert_file 'app/views/layouts/application.html.erb' do |content|
-      assert_equal 1, content.scan('devise_turnstile_meta_tag').size
+      assert_equal layout, content
     end
-  end
-
-  def test_skips_layout_injection_when_layout_missing
-    FileUtils.mkdir_p(File.join(destination_root, 'config/initializers'))
-    File.write(File.join(destination_root, 'config/initializers/devise.rb'), "# devise\n")
-
-    run_generator
-
-    assert_file 'config/initializers/cloudflare_turnstile.rb'
-    assert_no_file 'app/views/layouts/application.html.erb'
   end
 end
